94b0534b82769567fcf1877fbf010fe2abdb74b9
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-07-14 21:13:06 +0530

    Graph technology in AI and Data Science

---
+++ test_neo4j.py
@@ -0,0 +1,14 @@
+from neo4j import GraphDatabase
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+driver = GraphDatabase.driver(
+    os.getenv("NEO4J_URI"),
+    auth=(os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD")),
+)
+
+driver.verify_connectivity()
+print("Connected successfully!")
+driver.close()
